8966bce7c34fd6068e6906681c24aee25f4282e4
monish <monishbarse9@gmail.com>
2024-02-26 18:52:34 +0530

    same tab after closeing added

--- src/App.js
+++ src/App.js
@@ -1,46 +1,15 @@
 import React, { useEffect, useState } from "react";
-import {  Tabs } from "antd";
-import Trending from "./components/trending";
-import SearchItems from "./components/search";
+import MainTabs from "./Main";
 const App = () => {
+  const [activeKey, setActiveKey] = useState(1);
 
-  const [activeKey,setActiveKey] = useState(1)
-  let items = [
-    {
-      label: `Gifs `,
-      key: 1,
-      children: <Trending type='gifs'/>,
-    },
-    {
-      label: `Stickers `,
-      key: 2,
-      children: <Trending type='stickers'/>,
-    },
-    {
-      label: `Search`,
-      key: 3,
-      children: <SearchItems type='gifs'/>,
-    },
-  ];
-const handleOnChange = (activeKey)=>{
-  localStorage.setItem("activeKey",activeKey)
-  setActiveKey(activeKey)
-}
-const getActiveKey =  ()=>{
-  let key =  localStorage.getItem("activeKey")
-  setActiveKey(key)
-
-}
-useEffect(()=>{
-  getActiveKey()
-},[])
-  return (
-    <Tabs
-      defaultActiveKey={ activeKey || 1}
-      centered
-      items={items}
-      onChange={handleOnChange}
-    />
-  );
+  const getActiveKey = () => {
+    let key = localStorage.getItem("activeKey");
+    setActiveKey(parseInt(key));
+  };
+  useEffect(() => {
+    getActiveKey();
+  }, []);
+  return <MainTabs activeKey={activeKey} setActiveKey={setActiveKey} />;
 };
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Trending from "./components/trending";
+import SearchItems from "./components/search";
+import { Tabs } from "antd";
+
+const MainTabs = ({activeKey,setActiveKey}) => {
+  let items = [
+    {
+      label: `Gifs `,
+      key: 1,
+      children: <Trending type="gifs" />,
+    },
+    {
+      label: `Stickers `,
+      key: 2,
+      children: <Trending type="stickers" />,
+    },
+    {
+      label: `Search`,
+      key: 3,
+      children: <SearchItems type="gifs" />,
+    },
+  ];
+  const handleOnChange = (Key) => {
+    localStorage.setItem("activeKey", Key);
+    setActiveKey(Key);
+  };
+
+
+  return (
+    <Tabs
+      defaultActiveKey={activeKey || 1}
+      centered
+      items={items}
+      onChange={handleOnChange}
+      activeKey={activeKey}
+    />
+  );
+};
+
+export default MainTabs;
